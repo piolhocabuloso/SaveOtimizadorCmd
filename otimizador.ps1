b33b319a149be281ae3e3957974e15a55b1ff899
Piolho <165446707+piolhocabuloso@users.noreply.github.com>
2025-03-15 14:57:50 -0300

    Delete otimizador.ps1

--- otimizador.ps1
+++
@@ -1,8 +0,0 @@
-# Desativar aplicativos em segundo plano
-Get-AppxPackage | Remove-AppxPackage -ErrorAction SilentlyContinue
-
-# Desativar telemetria
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v AllowTelemetry /t REG_DWORD /d 0 /f
-
-# Limpeza de arquivos temporários
-cleanmgr /sagerun:1
